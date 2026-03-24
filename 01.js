5ccf270c92b89ff0d5a7eb484e20a1eafa62d61e
Raí Costa <rai.c@aluno.senai.br>
2026-03-24 13:28:08 -0300

    Add files via upload

---
+++ 01.js
@@ -0,0 +1,8 @@
+let cosmoBase = 100;
+
+// Aplicando as operações
+cosmoBase *= 5; // multiplica por 5
+cosmoBase -= 50; // subtrai 50
+
+console.log("Exercício 1 - cosmoBase final:", cosmoBase);
+// Resultado esperado: 450
---
+++ 02.js
@@ -0,0 +1,9 @@
+let armadura = false;
+let cosmoShiryu = 8000;
+let cosmoInimigo = 7500;
+
+// Expressão lógica
+let vitoria = !armadura && cosmoShiryu > cosmoInimigo;
+
+console.log("Exercício 2 - Vitória:", vitoria);
+// Resultado esperado: true
